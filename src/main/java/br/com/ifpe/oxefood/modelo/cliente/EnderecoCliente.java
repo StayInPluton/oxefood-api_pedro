4812f0faf2c4db4f43ef1b574b96a38aa58d5b1a
Lana <laisa0562@gmail.com>
2026-04-15 20:13:12 -0300

    Feat: Aula 15/04 sobre relacionamentos

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/EnderecoCliente.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/EnderecoCliente.java
@@ -1,48 +1,57 @@
-package br.com.ifpe.oxefood.modelo.produto;
+package br.com.ifpe.oxefood.modelo.cliente;
+
+import java.time.LocalDate;
 
 import org.hibernate.annotations.SQLRestriction;
 
 import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
 
 @Entity
-@Table(name = "Produto")
+@Table(name = "EnderecoCliente")
 @SQLRestriction("habilitado = true")
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Produto extends EntidadeAuditavel  {
 
+public class EnderecoCliente extends EntidadeAuditavel  {
+    @JsonIgnore
    @ManyToOne
-   private CategoriaProduto categoria;
-
-  
-   @Column
-   private String codigo;
+   private Cliente cliente;
 
    @Column
-   private String titulo;
+   private String rua;
 
    @Column
-   private String descricao;
+   private String numero;
 
    @Column
-   private Double valorUnitario;
+   private String bairro;
 
    @Column
-   private Integer tempoEntregaMinimo;
+   private String cep;
 
    @Column
-   private Integer tempoEntregaMaximo;
+   private String cidade;
+
+   @Column
+   private String estado;
+
+   @Column
+   private String complemento;
+
 
 }
